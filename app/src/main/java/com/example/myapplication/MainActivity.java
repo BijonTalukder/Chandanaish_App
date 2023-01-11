9c2e786e539801729592973a42a39336994ba420
BijonTalukder <81519797+BijonTalukder@users.noreply.github.com>
2023-01-11 23:11:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+//    private androidx.drawerlayout.widget.DrawerLayout DrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        nav = findViewById(R.id.navmenu);
+         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+         toggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+         drawerLayout.addDrawerListener(toggle);
+         toggle.syncState();
+         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+             @Override
+             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                 switch (item.getItemId())
+                 {
+                     case R.id.menu_home:
+                         drawerLayout.closeDrawer(GravityCompat.START);
+                         break;
+                     case R.id.setting:
+                         drawerLayout.closeDrawer(GravityCompat.START);
+                         break;
+                 }
+
+
+
+                 return true;
+             }
+         });
+
+    }
+}
