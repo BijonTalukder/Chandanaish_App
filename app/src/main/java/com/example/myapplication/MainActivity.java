c9f5d99eebcacae9fa0a5976033fff93e6f0a3f2
BijonTalukder <81519797+BijonTalukder@users.noreply.github.com>
2023-02-05 11:05:54 +0600

    Inflater added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContentInfo;
@@ -19,6 +21,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
@@ -27,15 +30,19 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+    int icon[]={R.drawable.doctor,R.drawable.education,R.drawable.medical};
     NavigationView nav;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     LinearLayout layout1;
     GridView gridView;
+    ArrayList< HashMap<String,String>> arrayList= new ArrayList<>();
+    HashMap<String,String> hashMap;
 //    private androidx.drawerlayout.widget.DrawerLayout DrawerLayout;
 
     @Override
@@ -52,6 +59,13 @@ public class MainActivity extends AppCompatActivity {
          toggle.syncState();
          MyAdapter myAdapter= new MyAdapter();
          gridView.setAdapter(myAdapter);
+         hashMap=new HashMap<>();
+         hashMap.put("name","R.drawble.");
+         arrayList.add(hashMap);
+
+        hashMap=new HashMap<>();
+        hashMap.put("name","on");
+        arrayList.add(hashMap);
         ImageSlider imageSlider;
 
 
@@ -97,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 6;
+            return arrayList.size();
         }
 
         @Override
@@ -114,6 +128,19 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int i, View view, ViewGroup viewGroup) {
             LayoutInflater layoutInflater= (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view1= layoutInflater.inflate(R.layout.item,viewGroup,false);
+            CardView cardView= view1.findViewById(R.id.cardview);
+            TextView Title = view1.findViewById(R.id.title_name);
+            HashMap<String,String> hashMap = arrayList.get(i);
+            String name= hashMap.get("name");
+            Title.setText(name);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent= new Intent(MainActivity.this,DetailsList.class);
+                    startActivity(intent);
+
+                }
+            });
             return view1;
         }
     }
